25bb37aa966a10088bd751f45522f889704a5f4c
ueno2025 <ken825325@outlook.jp>
2026-05-11 22:35:45 +0900

    add input text

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
 const input = document.querySelector(".todo-input");
 const button = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 button.addEventListener("click", () => {
+    const input_text = input.value;
+    if (input_text === "") return;
+
     console.log(input.value);
+    
+    const li = document.createElement("li");
+    li.textContent = input_text;
+    todoList.appendChild(li);
+    
     input.value = "";
 });
