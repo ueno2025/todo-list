2081fac30f353c3734bdb9f7dc6cad6dda337288
ueno2025 <ken825325@outlook.jp>
2026-05-13 21:54:41 +0900

    add function local storage

--- script.js
+++ script.js
@@ -4,23 +4,60 @@ const input_button = document.querySelector(".todo-btn");
 const todoList = document.querySelector(".todo-list");
 const delete_buttons = document.querySelectorAll(".delete-btn");
 
+
+// ローカルストレージにデータ保存
+function saveData() {
+    const lists = document.querySelectorAll("li");
+    let todos = [];
+    lists.forEach(li => {
+        todos.push(li.innerText);
+    })
+    localStorage.setItem("todoList", JSON.stringify(todos));
+}
+
+
+// ロード時のデータ読み込み
+window.addEventListener("load", () => {
+    const savedTodos = JSON.parse(localStorage.getItem("todoList"));
+
+    if (savedTodos && savedTodos.length > 0) {
+        savedTodos.forEach(todoText => {
+            renderTodo(todoText);
+        });
+    } else {
+        const defaultTodos = ["todo-1", "todo-2", "todo-3"];
+        defaultTodos.forEach(todoText => {
+            renderTodo(todoText);
+        });
+        saveData();
+    }
+});
+
+function renderTodo(text) {
+    const li = document.createElement("li");
+    li.classList.add('todo-li');
+    li.textContent = text;
+
+    // 削除ボタン作成
+    const delete_button = document.createElement("button");
+    delete_button.innerHTML = '<img src="trash-2.svg" alt="削除">';
+    delete_button.addEventListener("click", () => {
+        li.remove();
+        saveData();
+    });
+    li.appendChild(delete_button);
+    
+    todoList.appendChild(li);
+}
+
+
 // 入力処理
 function addTodo() {
     const input_text = input.value;
     if (input_text === "") return;
 
-    const li = document.createElement("li");
-    li.classList.add('todo-li');
-    li.textContent = input_text;
-
-    const delete_button = document.createElement("button");
-    delete_button.innerHTML = '<img src="trash-2.svg" alt="削除">';
-    delete_button.addEventListener("click", () => {
-        li.remove();
-    });
-    li.appendChild(delete_button);
-    
-    todoList.appendChild(li);
+    renderTodo(input_text);
+    saveData();
 
     input.value = "";
 }
